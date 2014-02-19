bb9416e9cf91cc721e5fc18f09c23bb206200aca
Duc Nguyen <vietducnguyen.vn@gmail.com>
2014-02-18 19:00:08 -0500

    leftmost-child right-sibling tree class declaration

---
+++ tree/lcrs_tree.cpp
@@ -0,0 +1,11 @@
+// Tree class: Leftmost-child Right-sibling Implementation
+#include "lcrs_tree.h"
+
+CSTree::CSTree() {
+	root = -1;
+	cellspace = new Cellspace;
+}
+
+CSTree::~CSTree() {
+	// TODO deallocate memory here
+}
---
+++ tree/lcrs_tree.h
@@ -0,0 +1,15 @@
+// Leftmost-child Right-sibling representation of tree
+// Tree class declaration
+#ifndef __LCRS_TREE_H_
+#define __LCRS_TREE_H_
+
+class CSTree {
+private:
+	int root;
+	Cellspace cellspace;
+public:
+	Cellspace();
+	~Cellspace();
+};
+
+#endif
